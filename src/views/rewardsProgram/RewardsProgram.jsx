a01591a95def5c796da1b61c0ee171e410882cd6
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2022-09-28 15:06:14 -0400

    adjusted rewards program component

--- src/views/rewardsProgram/RewardsProgram.jsx
+++ src/views/rewardsProgram/RewardsProgram.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Header } from '../../components'
 
+import './rewardsProgram.css'
+/**
+ * This component is the base view. 
+ * @returns React.ReactNode
+ */
 const RewardsProgram = () => {
 	return (
-		<div className="">
-
+		<div className="rewardsProgramWrapper">
+			<Header />
+			{/* <UserRewards /> */}
 		</div>
 	)
 }
